db7b7074a8e9353a0b68007e308fd395de3a0623
lukew3 <lukew25073@gmail.com>
2021-06-27 14:07:28 -0400

    removed upload to server, changed to save locally

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import praw
 import requests
 import json
+import uuid
 
 r = praw.Reddit()
 
 API_KEY = open("api-key.txt", "r").read().strip()
+SAVE_LOCATION = "/home/luke/lukew3-com/recolorbot/photos/"
 
 def make_response(message):
     print("Processing request...")
@@ -23,13 +25,17 @@ def make_response(message):
             return "An error occurred during colorization. Original post must be an image or a direct link to an image. You can still generate a colorized image by downloading and uploading manually to [DeepAI Image Colorizer](https://deepai.org/machine-learning-model/colorizer)"
     except Exception:
         new_image_url = (r.json())["output_url"]
-        # Upload to my server
-        r2 = requests.post(f"https://lukew3.com/recolorbot/upload?url={new_image_url}")
-        final_image_url = (r2.json())["url"]
+        # Save locally
+        r2 = requests.get(url)
+        filename = str(uuid.uuid4()) + ".jpg"
+        with open(SAVE_LOCATION + filename, 'wb') as f:
+            f.write(r2.content)
+        # The photo will not be available via the following link if the program is not running on the same server as my website
+        final_image_url = f"https://lukew3.com/recolorbot/photos/{filename}"
         return f"[Colorized image]({final_image_url})"
 
-#footer = "\n\n ^( [dev](https://github.com/lukew3/)|[source](https://github.com/lukew3/recolorbot) )" 
-footer = "\n\n ^( developed by [lukew3](https://github.com/lukew3/) )" 
+footer = "\n\n ^( [dev](https://github.com/lukew3/)|[source](https://github.com/lukew3/recolorbot) )" 
+#footer = "\n\n ^( developed by [lukew3](https://github.com/lukew3/) )" 
 messages = r.inbox.stream()
 print("Awaiting requests")
 for message in messages:
