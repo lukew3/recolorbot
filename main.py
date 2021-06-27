3e026ca63c5fe915eb38148c689cfcd740357c01
lukew3 <lukew25073@gmail.com>
2021-06-26 23:10:56 -0400

    Upload images to personal server

--- main.py
+++ main.py
@@ -23,9 +23,13 @@ def make_response(message):
             return "An error occurred during colorization. Original post must be an image or a direct link to an image. You can still generate a colorized image by downloading and uploading manually to [DeepAI Image Colorizer](https://deepai.org/machine-learning-model/colorizer)"
     except Exception:
         new_image_url = (r.json())["output_url"]
-        return f"[Colorized image]({new_image_url})"
+        # Upload to my server
+        r2 = requests.post(f"https://lukew3.com/recolorbot/upload?url={new_image_url}")
+        final_image_url = (r2.json())["url"]
+        return f"[Colorized image]({final_image_url})"
 
-footer = "\n\n ^( [dev](https://github.com/lukew3/)|[source](https://github.com/lukew3/recolorbot) )" 
+#footer = "\n\n ^( [dev](https://github.com/lukew3/)|[source](https://github.com/lukew3/recolorbot) )" 
+footer = "\n\n ^( developed by [lukew3](https://github.com/lukew3/) )" 
 messages = r.inbox.stream()
 print("Awaiting requests")
 for message in messages:
@@ -34,6 +38,7 @@ for message in messages:
             message.reply(make_response(message) + footer)
             message.mark_read()
             print("Message sent")
-    except praw.exceptions.APIException:
+    except praw.exceptions.APIException as e:
+        print(e)
         print("Probably hit rate limit")
 
